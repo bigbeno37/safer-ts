8fdd1074b117a1eaf4a8ff8745b9c83f52e07de2
bigbeno37 <contact@benosullivan.com>
2022-12-13 12:44:04 +1100

    Added SafeWebSocket

---
+++ src/SafeWebSocket.ts
@@ -0,0 +1,35 @@
+import { io, IO } from './monads/IO';
+import { AsyncResult, Err, Ok, Result } from './monads/Result';
+import { z, ZodError, ZodSchema } from 'zod';
+import { parseJSON } from './SafeJSON';
+
+type CreateConnection<T extends { disconnect: () => IO<void> }, E> = (onDisconnect: () => void) => IO<AsyncResult<T, E>>;
+
+type SafeWebSocket<S extends ZodSchema, M> = {
+	listen: (listener: (message: ReturnType<typeof parseJSON>, rawMessage: string) => void) => void,
+	send: (message: M) => IO<void>,
+	disconnect: () => IO<void>
+};
+
+export const createSafeWebSocket = <S extends ZodSchema, M>(url: string, messageSchema: S): CreateConnection<SafeWebSocket<S, M>, Event> => onDisconnect => {
+	return io(() => {
+		const ws = new WebSocket(url);
+
+		return new Promise((resolve, reject) => {
+			ws.addEventListener('open', () => {
+				ws.addEventListener('close', onDisconnect);
+
+				const safeWebSocket: SafeWebSocket<S, M> = {
+					listen: listener => ws.addEventListener('message', ({data}) => {
+						listener(parseJSON(data, messageSchema), data);
+					}),
+					send: message => io(() => ws.send(JSON.stringify(message))),
+					disconnect: () => io(() => ws.close())
+				};
+
+				resolve(Ok(safeWebSocket));
+			});
+			ws.addEventListener('error', (e) => reject(Err(e)))
+		});
+	});
+};
