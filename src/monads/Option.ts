916b6fce7ecf025f7e7cdb7962159630fdbdc54f
bigbeno37 <contact@benosullivan.com>
2022-12-15 23:37:04 +1100

    Updated dependencies; Added caseOf to Result and Option; Migrated helper functions into separate type for Result and Option for better typedoc documentation

--- src/monads/Option.ts
+++ src/monads/Option.ts
@@ -1,74 +1,77 @@
 import { Err, Ok, Result } from './Result';
 import type { SafeMap } from '../datastructures';
 
+type OptionHelpers<T> = {
+	/**
+	 * Returns whether this Option is a Some, containing an inner value.
+	 */
+	isSome: () => boolean,
+
+	/**
+	 * Returns whether this Option is a None, containing no value.
+	 */
+	isNone: () => boolean,
+
+	/**
+	 * Maps this Option's inner value if it is a Some, otherwise returning this Option, allowing for chaining.
+	 *
+	 * @param mapper The mapper to be called if this is a Some.
+	 */
+	map: <U>(mapper: (data: T) => U) => Option<U>,
+
+	/**
+	 * Maps this Option's inner value into a new Option if it is a Some, allowing for multiple Options to be
+	 * chained together without massive indenting.
+	 *
+	 * @param chainer The function that will be called to map this Option's data if it is a Some.
+	 */
+	andThen: <U>(chainer: (data: T) => Option<U>) => Option<U>,
+
+	/**
+	 * Maps this Option into a new Option if this is None, otherwise returning this Option.
+	 *
+	 * @param chainer The function that will be called to map into a new Option if this is a None.
+	 */
+	orElse: <U>(chainer: () => Option<U>) => Option<U>,
+
+	/**
+	 * Unwraps this Option's inner value if it is a Some, OTHERWISE THROWS AN ERROR!
+	 *
+	 * NOTE: This is an escape hatch for imperative code; be sure to check that this is a Some before calling this!
+	 * It is recommended to make use of {@link Option#map}, {@link Option#andThen}, and {@link Option#inspect} instead.
+	 */
+	unwrap: () => T,
+
+	/**
+	 * Calls the given function with this Option's data if this is a Some, otherwise does nothing and returns this Option.
+	 *
+	 * @param fn The function that will be called if this is a Some.
+	 */
+	inspect: (fn: (data: T) => void) => Option<T>,
+
+	/**
+	 * Converts this Option into a {@link Result}. If this is a Some, this will be converted into an Ok, whereas
+	 * if this is a None, this will be converted into an Err of the given error.
+	 *
+	 * @param error The error to be used if this is a None during conversion into a Result.
+	 */
+	okOr: <E>(error: E) => Result<T, E>,
+
+	/**
+	 * Calls either Some or None based on whether this Option is a Some or a None.
+	 *
+	 * @param matcher An object containing what to be returned based on the type of this Option.
+	 */
+	match: <U>(matcher: { Some: (data: T) => U, None: () => U }) => U,
+
+	caseOf: <U>(cases: { Some: (data: T) => U, None: () => U }) => U
+};
+
 /**
  * Represents an optional value, effectively null on steroids. This can be used to represent the absence of a value,
  * such as retrieving a value based on a key in a {@link SafeMap}, while preventing null pointer exceptions outright.
  */
-export type Option<T> = ({ type: 'some', data: T } | { type: 'none' })
-	& {
-		/**
-		 * Returns whether this Option is a Some, containing an inner value.
-		 */
-		isSome: () => boolean,
-
-		/**
-		 * Returns whether this Option is a None, containing no value.
-		 */
-		isNone: () => boolean,
-
-		/**
-		 * Maps this Option's inner value if it is a Some, otherwise returning this Option, allowing for chaining.
-		 *
-		 * @param mapper The mapper to be called if this is a Some.
-		 */
-		map: <U>(mapper: (data: T) => U) => Option<U>,
-
-		/**
-		 * Maps this Option's inner value into a new Option if it is a Some, allowing for multiple Options to be
-		 * chained together without massive indenting.
-		 *
-		 * @param chainer The function that will be called to map this Option's data if it is a Some.
-		 */
-		andThen: <U>(chainer: (data: T) => Option<U>) => Option<U>,
-
-		/**
-		 * Maps this Option into a new Option if this is None, otherwise returning this Option.
-		 *
-		 * @param chainer The function that will be called to map into a new Option if this is a None.
-		 */
-		orElse: <U>(chainer: () => Option<U>) => Option<U>,
-
-		/**
-		 * Unwraps this Option's inner value if it is a Some, OTHERWISE THROWS AN ERROR!
-		 *
-		 * NOTE: This is an escape hatch for imperative code; be sure to check that this is a Some before calling this!
-		 * It is recommended to make use of {@link Option#map}, {@link Option#andThen}, and {@link Option#inspect} instead.
-		 */
-		unwrap: () => T,
-
-		/**
-		 * Calls the given function with this Option's data if this is a Some, otherwise does nothing and returns this Option.
-		 *
-		 * @param fn The function that will be called if this is a Some.
-		 */
-		inspect: (fn: (data: T) => void) => Option<T>,
-
-		/**
-		 * Converts this Option into a {@link Result}. If this is a Some, this will be converted into an Ok, whereas
-		 * if this is a None, this will be converted into an Err of the given error.
-		 *
-		 * @param error The error to be used if this is a None during conversion into a Result.
-		 */
-		okOr: <E>(error: E) => Result<T, E>,
-
-		/**
-		 * Calls either Some or None based on whether this Option is a Some or a None.
-		 *
-		 * @param matcher An object containing what to be returned based on the type of this Option.
-		 */
-		match: <U>(matcher: { Some: (data: T) => U, None: () => U }) => U
-	};
+export type Option<T> = ({ type: 'some', data: T } | { type: 'none' }) & OptionHelpers<T>;
 
 /**
  * Returns whether the given {@link Option} is a Some, and provides type narrowing, which may be preferable over
@@ -126,6 +129,9 @@ const createOption = <T>(obj: { type: 'some', data: T } | { type: 'none' }): Opt
 	},
 	match<U>(matcher: { Some: (data: T) => U, None: () => U }) {
 		return isSome(this) ? matcher.Some(this.data) : matcher.None();
+	},
+	caseOf<U>(cases: { Some: (data: T) => U, None: () => U }): U {
+		return isSome(this) ? cases.Some(this.data) : cases.None();
 	}
 });
 
--- src/monads/Result.ts
+++ src/monads/Result.ts
@@ -1,80 +1,83 @@
+type ResultHelpers<T, E> = {
+	/**
+	 * Returns whether this Result is an Ok.
+	 */
+	isOk: () => boolean,
+
+	/**
+	 * Returns whether this Result is an Err.
+	 */
+	isErr: () => boolean,
+
+	/**
+	 * Maps this Result's data if this is an Ok, otherwise does nothing and returns this Result.
+	 *
+	 * @param mapper A mapping function that will map this Result's data if this is Ok.
+	 */
+	map: <U>(mapper: (data: T) => U) => Result<U, E>,
+
+	/**
+	 * Maps this Result's error if this is an Err, otherwise does nothing and returns this Result.
+	 *
+	 * @param mapper A mapping function that will map this Result's error if this is Err.
+	 */
+	mapErr: <U>(mapper: (error: E) => U) => Result<T, U>,
+
+	/**
+	 * Maps this Result's data into a new Result if this is an Ok, otherwise does nothing and returns this Result.
+	 * Useful for chaining multiple operations that may fail together without massive indentation.
+	 *
+	 * @param chainer A function that will map this Result's data into a new Result if this is Ok.
+	 */
+	andThen: <U>(chainer: (data: T) => Result<U, E>) => Result<U, E>,
+
+	/**
+	 * Maps this Result's error into a new Result if this is an Err, otherwise does nothing and returns this Result.
+	 *
+	 * @param chainer A function that will map this Result's error into a new Result if this is Err.
+	 */
+	orElse: <U>(chainer: (error: E) => Result<T, U>) => Result<T, U>,
+
+	/**
+	 * Returns this Result's data if this is Ok, OTHERWISE THROWS AN ERROR.
+	 *
+	 * NOTE: This is an escape hatch for imperative code; be sure to check that this is an Ok before calling this!
+	 * It is recommended to make use of {@link Result#map}, {@link Result#andThen}, and {@link Result#inspect} instead.
+	 */
+	unwrap: () => T,
+
+	/**
+	 * Returns this Result's error if this is Err, OTHERWISE THROWS AN ERROR.
+	 *
+	 * NOTE: This is an escape hatch for imperative code; be sure to check that this is an Err before calling this!
+	 * It is recommended to make use of {@link Result#mapErr}, {@link Result#orElse}, and {@link Result#inspectErr} instead.
+	 */
+	unwrapErr: () => E,
+
+	/**
+	 * Runs the given function if this is an Ok, otherwise does nothing and returns this Result.
+	 *
+	 * @param fn The function to be run.
+	 */
+	inspect: (fn: (data: T) => void) => Result<T, E>,
+
+	/**
+	 * Runs the given function if this is an Err, otherwise does nothing and returns this Result.
+	 *
+	 * @param fn The function to be run.
+	 */
+	inspectErr: (fn: (error: E) => void) => Result<T, E>,
+
+	caseOf: <U>(cases: { Ok: (data: T) => U, Err: (error: E) => U }) => U
+};
+
 /**
  * Represents the result of an operation, containing either a successful value or an error that occurred. This can be
  * useful in situations where an Error might otherwise be used, such as parsing JSON. This is especially useful in
  * JavaScript, where Errors may be thrown at any level and aren't documented. Returning a Result, on the other hand,
  * is explicit in the code itself and is fully type-safe.
  */
-export type Result<T, E> = ({ type: 'ok', data: T } | { type: 'err', error: E })
-	& {
-		/**
-		 * Returns whether this Result is an Ok.
-		 */
-		isOk: () => boolean,
-
-		/**
-		 * Returns whether this Result is an Err.
-		 */
-		isErr: () => boolean,
-
-		/**
-		 * Maps this Result's data if this is an Ok, otherwise does nothing and returns this Result.
-		 *
-		 * @param mapper A mapping function that will map this Result's data if this is Ok.
-		 */
-		map: <U>(mapper: (data: T) => U) => Result<U, E>,
-
-		/**
-		 * Maps this Result's error if this is an Err, otherwise does nothing and returns this Result.
-		 *
-		 * @param mapper A mapping function that will map this Result's error if this is Err.
-		 */
-		mapErr: <U>(mapper: (error: E) => U) => Result<T, U>,
-
-		/**
-		 * Maps this Result's data into a new Result if this is an Ok, otherwise does nothing and returns this Result.
-		 * Useful for chaining multiple operations that may fail together without massive indentation.
-		 *
-		 * @param chainer A function that will map this Result's data into a new Result if this is Ok.
-		 */
-		andThen: <U>(chainer: (data: T) => Result<U, E>) => Result<U, E>,
-
-		/**
-		 * Maps this Result's error into a new Result if this is an Err, otherwise does nothing and returns this Result.
-		 *
-		 * @param chainer A function that will map this Result's error into a new Result if this is Err.
-		 */
-		orElse: <U>(chainer: (error: E) => Result<T, U>) => Result<T, U>,
-
-		/**
-		 * Returns this Result's data if this is Ok, OTHERWISE THROWS AN ERROR.
-		 *
-		 * NOTE: This is an escape hatch for imperative code; be sure to check that this is an Ok before calling this!
-		 * It is recommended to make use of {@link Result#map}, {@link Result#andThen}, and {@link Result#inspect} instead.
-		 */
-		unwrap: () => T,
-
-		/**
-		 * Returns this Result's error if this is Err, OTHERWISE THROWS AN ERROR.
-		 *
-		 * NOTE: This is an escape hatch for imperative code; be sure to check that this is an Err before calling this!
-		 * It is recommended to make use of {@link Result#mapErr}, {@link Result#orElse}, and {@link Result#inspectErr} instead.
-		 */
-		unwrapErr: () => E,
-
-		/**
-		 * Runs the given function if this is an Ok, otherwise does nothing and returns this Result.
-		 *
-		 * @param fn The function to be run.
-		 */
-		inspect: (fn: (data: T) => void) => Result<T, E>,
-
-		/**
-		 * Runs the given function if this is an Err, otherwise does nothing and returns this Result.
-		 *
-		 * @param fn The function to be run.
-		 */
-		inspectErr: (fn: (error: E) => void) => Result<T, E>
-	};
+export type Result<T, E> = ({ type: 'ok', data: T } | { type: 'err', error: E }) & ResultHelpers<T, E>;
 
 /**
  * Returns whether the given {@link Result} is an Ok, and provides type narrowing. This may be preferable over
@@ -139,6 +142,9 @@ const createResult = <T, E>(obj: { type: 'ok', data: T } | { type: 'err', error:
 		}
 
 		return this;
+	},
+	caseOf<U>(cases: { Ok: (data: T) => U, Err: (error: E) => U }): U {
+		return isOk(this) ? cases.Ok(this.data) : cases.Err(this.error);
 	}
 });
 
